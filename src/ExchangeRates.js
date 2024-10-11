e5d10a360f47ec5d78eae0722ac0a8c9cab03d79
senguiz <izguseniz24@gmail.com>
2024-10-11 06:53:36 +0300

    c

--- src/ExchangeRates.js
+++ src/ExchangeRates.js
@@ -4,12 +4,12 @@ const API_KEY = 'f9db03f08ae6078aaea4b03a';
 const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest`;
 
 export const getExchangeRates = async (baseCurrency) => {
-  
+
   try {
     const response = await axios.get(`${BASE_URL}/${baseCurrency}`);
     return response.data;
   } 
   catch (error) {
-    console.error("API error:", error);
+    console.error("Error:", error);
   }
 };
